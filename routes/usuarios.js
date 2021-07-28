474a2edb7c2a835cdf4f93a40d2c01f6772dfaa2
rafael romero <rafael_david_romero@hotmail.com>
2021-07-28 17:26:55 -0300

    Actualizacion 28/07

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -10,8 +10,8 @@ const usuarioController = require('../controllers/usuarioController');
 // URL: api/usuarios
 router.post('/',[
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('email', 'Agrega un email valido').not().isEmail(),
-    check('password', 'El password debe ser minimo de 6 caracteres').not().isLength({min:6})
+    check('email', 'Agrega un email valido').isEmail(),
+    check('password', 'El password debe ser minimo de 6 caracteres').isLength({min:6})
 ],
     usuarioController.crearUsuario
 );
